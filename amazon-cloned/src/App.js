f21b72c00ca03d767cf37dbea066cd1615c46df5
Rozanne-o <rozanowino@gmail.com>
2024-01-07 21:54:33 +0300

    update

---
+++ amazon-cloned/src/App.js
@@ -0,0 +1,17 @@
+import React from "react";
+import './App.css';
+import Header from './Header';
+
+function App() {
+  return (
+    //BEM
+    <div className="App">
+      {/* rendering Header */}
+      <Header />
+      {/* Home */}
+
+    </div>
+  );
+}
+
+export default App;
---
+++ amazon-cloned/src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyAo-NPhb0j4yCSxYZRepqVv7r8zq-FzBWI",
+    authDomain: "challenge-d4a2a.firebaseapp.com",
+    projectId: "challenge-d4a2a",
+    storageBucket: "challenge-d4a2a.appspot.com",
+    messagingSenderId: "326619141776",
+    appId: "1:326619141776:web:e3bdcb570013398f172a15",
+    measurementId: "G-NESWCQRFJJ"
+  };
